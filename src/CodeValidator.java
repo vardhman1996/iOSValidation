cdbd5cf114376da7ee6d90e15818426829078aba
Vardhman Mehta <vardhman.mehta96@gamil.com>
2015-07-23 17:48:48 +0530

    Added basic files

---
+++ src/CodeValidator.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+/**
+ * Created by VardhmanMehta on 23/07/15.
+ */
+public class CodeValidator {
+    public static void main(String[] args) {
+        File file = new File("/Users/VardhmanMehta/Desktop/testXCode/testCode/testCode/AppDelegate.m");
+        
+    }
+}
